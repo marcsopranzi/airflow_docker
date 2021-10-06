3244f32c49a020675ed48e4d626b6a7a41c9beca
marcsopranzi <marcsopranzi@gmail.com>
2021-10-06 21:58:43 +0100

    forex pipeline

--- README.md
+++
@@ -1,2 +0,0 @@
-# airflow_docker
-# to do
--- docker_setup/create_docker.sh
+++ docker_setup/create_docker.sh
@@ -5,3 +5,9 @@ sudo docker run -it -p 8080:8080 build_linux_airflow /bin/bash
 sudo docker run -it --rm -d -p 8080:8080 build_linux_airflow
 
 sudo docker ps
+sudo docker kill bd45e9bdd437
+
+sudo docker images
+sudo docker rmi -f c5ac86982355
+
+sudo docker exec -it 9c3e356fbbae /bin/bash
